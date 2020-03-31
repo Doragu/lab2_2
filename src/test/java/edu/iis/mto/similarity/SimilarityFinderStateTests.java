af854328003966f3d6ed66f77fa63c79e10a180e
Krzysztof Chybicki <krysiek97@interia.eu>
2020-03-31 21:57:59 +0200

    zaimplementowanie testu stanu testTwoIdenticalSequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTests.java
@@ -1,11 +1,25 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SimilarityFinderStateTests {
 
-    @Test void testTwoIdenticalSequences() {
+    private final static int[] ORIGIN_SEQUENCE = {1, 2, 3};
 
+    private SequenceSearcher sequenceSearcher;
+    private SimilarityFinder similarityFinder;
+
+    @BeforeEach void createNewSequenceSearcher() {
+        sequenceSearcher = new SequenceSearcherDubler();
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+    }
+
+    @Test void testTwoIdenticalSequences() {
+        Assertions.assertEquals(similarityFinder.calculateJackardSimilarity(ORIGIN_SEQUENCE, ORIGIN_SEQUENCE.clone()), 1);
     }
 
     @Test void testSequencesWithOneDifferentElement() {
