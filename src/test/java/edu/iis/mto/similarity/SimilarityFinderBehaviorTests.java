23ff5a2f8c67ffd889f19d98e356d38ba0b753b4
Krzysztof Chybicki <krysiek97@interia.eu>
2020-03-31 22:14:55 +0200

    zaimplementowanie testu zachowania testWithTwoNullSequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
@@ -1,11 +1,25 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SimilarityFinderBehaviorTests {
 
-    @Test void testWithTwoNullSequences() {
+    private SimilarityFinder similarityFinder;
 
+    @BeforeEach void createNewSequenceSearcher() {
+        SequenceSearcher sequenceSearcher = new SequenceSearcherDubler();
+        similarityFinder = new SimilarityFinder(sequenceSearcher);
+    }
+
+    @Test void testWithTwoNullSequences() {
+        final int seq1[] = null, seq2[] = null;
+
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        });
     }
 
     @Test void testWithOneNullSequence() {
@@ -16,13 +30,14 @@ public class SimilarityFinderBehaviorTests {
 
     }
 
-    @Test void testWithTwoEmptySequences() {
+    @Test
+
+    void testWithTwoEmptySequences() {
 
     }
 
     @Test void testWithNullSequenceSearcher() {
 
     }
-
 
 }
