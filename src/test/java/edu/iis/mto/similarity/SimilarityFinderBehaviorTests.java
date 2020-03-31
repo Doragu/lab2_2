3c357afb898557704f7599e3b1dbfc19613559aa
Krzysztof Chybicki <krysiek97@interia.eu>
2020-03-31 21:28:47 +0200

    projektowanie przypadków testowych

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviorTests.java
@@ -0,0 +1,28 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+public class SimilarityFinderBehaviorTests {
+
+    @Test void testWithTwoNullSequences() {
+
+    }
+
+    @Test void testWithOneNullSequence() {
+
+    }
+
+    @Test void testWithTwoFilledSequences() {
+
+    }
+
+    @Test void testWithTwoEmptySequences() {
+
+    }
+
+    @Test void testWithNullSequenceSearcher() {
+
+    }
+
+
+}
---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTests.java
@@ -0,0 +1,30 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+public class SimilarityFinderStateTests {
+
+    @Test void testTwoIdenticalSequences() {
+
+    }
+
+    @Test void testSequencesWithOneDifferentElement() {
+
+    }
+
+    @Test void testSequencesWithOneSameElement() {
+
+    }
+
+    @Test void testSequencesWithZeroSameElements() {
+
+    }
+
+    @Test void testWithTwoEmptySequences() {
+
+    }
+
+    @Test void testWithOneEmptySequence() {
+
+    }
+}
